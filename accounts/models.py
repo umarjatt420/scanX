43ea525cafb7e3813fe23a519e5e9f9d2950043a
Umar Jatt <umarjatt0420@gmail.com>
2023-02-24 02:12:13 +0500

    add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from .managers import CustomUserManager
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-    fullname = models.CharField(max_length=120)
+    full_name = models.CharField(max_length=120)
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
