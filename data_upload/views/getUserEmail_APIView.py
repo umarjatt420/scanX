7f1856997672b7c648380ea6ef3e5eaf0e9a247c
Umar Jatt <umarjatt0420@gmail.com>
2023-02-28 01:15:46 +0500

    add API for get user email

---
+++ data_upload/views/getUserEmail_APIView.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+
+class GetUserEmailView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    @staticmethod
+    def get(request):
+        user = request.user
+        return Response({
+            'email': user.email
+        }, status=status.HTTP_200_OK)
