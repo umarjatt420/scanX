550ea47b516d58b58cec4493d6690226da5d3c34
Umar Jatt <umarjatt0420@gmail.com>
2023-02-28 01:16:36 +0500

    add API for get Users List and permissions

---
+++ data_upload/views/get_users_APIView.py
@@ -0,0 +1,15 @@
+from accounts.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..serializers.getUsers_serializer import GetUsersSerializer
+from rest_framework import permissions, status
+
+class GetUsersView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    @staticmethod
+    def get(request, *args, **kwargs):
+        users = User.objects.filter(is_superuser=False, is_staff=False, is_active=True)
+        serializer = GetUsersSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
