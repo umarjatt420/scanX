53d2a996fd9a90bc586aa09062e9f360f8db0711
Umar Jatt <umarjatt0420@gmail.com>
2023-02-28 01:20:09 +0500

    update Register User API and add permissions in this API

--- data_upload/views/register_APIView.py
+++ data_upload/views/register_APIView.py
@@ -1,21 +1,12 @@
-from accounts.models import User
 from ..serializers.register_serializer import RegisterSerializer
-from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 
 
 # Create your views here.
-
-
-# class RegisterView(CreateAPIView):
-#     queryset = User.objects.all()
-#     permission_classes = (AllowAny,)
-#     serializer_class = RegisterSerializer
-
 class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
     @staticmethod
     def post(request):
         serializer = RegisterSerializer(data=request.data)
