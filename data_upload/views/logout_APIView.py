0588fd630a499d5068bfbd3c57b23861049fa316
Umar Jatt <umarjatt0420@gmail.com>
2023-02-28 01:17:49 +0500

    add permissions in logout API

--- data_upload/views/logout_APIView.py
+++ data_upload/views/logout_APIView.py
@@ -1,9 +1,10 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import logout
 
 class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     @staticmethod
     def get(request):
         logout(request)
