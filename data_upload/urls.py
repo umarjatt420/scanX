2a15efc3665829102567e4d38b6add519fc7f405
Umar Jatt <umarjatt0420@gmail.com>
2023-03-05 01:59:33 +0500

    add update user and delete user url in urls file

--- data_upload/urls.py
+++ data_upload/urls.py
@@ -21,6 +21,8 @@ from .views.logout_APIView import LogoutView
 from .views.upload_APIView import UploadView
 from .views.get_users_APIView import GetUsersView
 from .views.getUserEmail_APIView import GetUserEmailView
+from .views.delete_user_APIView import DeleteUserView
+from .views.update_user_APIView import UpdateUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -29,4 +31,6 @@ urlpatterns = [
     path('file-upload/', UploadView.as_view(), name='file-upload'),
     path('get-users/', GetUsersView.as_view(), name='get-users'),
     path('get-user-email/', GetUserEmailView.as_view(), name='get-user-email'),
+    path('delete-user/<int:pk>/', DeleteUserView.as_view(), name='delete-user'),
+    path('update-user/<int:pk>/', UpdateUserView.as_view(), name='update-user'),
 ]
