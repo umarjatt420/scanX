2e4e16b420f1b0fc2eb353bff44d3179c6601b4e
Umar Jatt <umarjatt0420@gmail.com>
2023-03-16 00:16:00 +0500

    add auth token in login api

--- data_upload/views/login_APIView.py
+++ data_upload/views/login_APIView.py
@@ -2,20 +2,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from ..serializers.login_serializer import LoginSerializer
-from django.middleware.csrf import get_token
 from django.contrib.auth import login
+from rest_framework.authtoken.models import Token
+
 
 class LoginView(APIView):
     permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
     @staticmethod
     def post(request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data['user']
-            csrf_token = get_token(request)
             login(request, user)
+            token, _ = Token.objects.get_or_create(user=user)
             return Response({
                 'success': True,
-                'csrf_token': csrf_token
+                'token': token.key,
                              })
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
