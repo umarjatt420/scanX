c6b6b244ec0106ddf925cea751d502ba21902cd8
Umar Jatt <umarjatt0420@gmail.com>
2023-03-16 00:14:49 +0500

    remove whitespace from login serializer

--- data_upload/serializers/login_serializer.py
+++ data_upload/serializers/login_serializer.py
@@ -8,11 +8,8 @@ class LoginSerializer(serializers.Serializer):
     def validate(self, attrs):
         email = attrs.get('email')
         password = attrs.get('password')
-
         user = authenticate(email=email, password=password)
-
         if not user:
             raise serializers.ValidationError('Invalid login details')
-
         attrs['user'] = user
         return attrs
